279a2c79705a72f210ccd76209e6fe5a49443596
Abbas Gheybizadeh <emaddeve@gmail.com>
2026-08-02 13:37:46 +0330

    update cli for recorder and run commands

--- src/swagtrace/cli.py
+++ src/swagtrace/cli.py
@@ -1,5 +1,9 @@
 import argparse
 from swagtrace.initializer import discover_and_save
+from swagtrace.recorder import run_server
+from swagtrace.tester import run_tests
+from swagtrace.consts import DEFAULT_YAML_FILE, DEFAULT_TEST_MODULE_FOLDER
+
 
 def main():
     
@@ -15,10 +19,26 @@ def main():
     init_parser.add_argument("-o", "--output", type=str, help="output path for files and folders", default=".")
     init_parser.set_defaults(func=discover_and_save)
 
+    recorder_parser = subparsers.add_parser("record", help="record requests API")
+    recorder_parser.add_argument("--host", type=str, help="API Base url", default="http://127.0.0.1:8000")
+    recorder_parser.add_argument("--port", type=int, help="Proxy Listen Port", default=8080)
+    recorder_parser.add_argument("--file", type=str, help="path of swagtrace.yaml file", default=DEFAULT_YAML_FILE)
+    recorder_parser.add_argument("--dir", type=str, help="path of test module", default=DEFAULT_TEST_MODULE_FOLDER)
+    recorder_parser.set_defaults(func=run_server)
+
+    tests_parser = subparsers.add_parser("run", help="run tests")
+    tests_parser.add_argument("--host", type=str, help="API Base url", default="http://127.0.0.1:8000")
+    tests_parser.add_argument("--file", type=str, help="path of swagtrace.yaml file", default=DEFAULT_YAML_FILE)
+    tests_parser.add_argument("--dir",  type=str, help="path of test module", default=DEFAULT_TEST_MODULE_FOLDER)
+    tests_parser.add_argument("--verbose",  type=bool, help="verbose", default=False)
+    
+    tests_parser.set_defaults(func=run_tests)
+
+
     args = parser.parse_args()
 
     if hasattr(args, "func"):
         kwargs = {arg[0]:arg[1] for arg in args._get_kwargs() if arg[0] != "command" and arg[0] != "func"} 
-        args.func(**kwargs)
+        return args.func(**kwargs)
     else:
         parser.print_help()
