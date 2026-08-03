1807ee18c5d4751748e594da73fc9ef221e4468f
Abbas Gheybizadeh <emaddeve@gmail.com>
2026-08-03 14:42:19 +0330

    improve tester messages and run reuqests

--- src/swagtrace/tester.py
+++ src/swagtrace/tester.py
@@ -1,64 +1,27 @@
-from typing import Any
 
-from types import ModuleType
 
-import requests
-import sys
 import subprocess
-import importlib.util
+import time
 
 from pathlib import Path
+
+from httpx import Client
 
 from yaml_syntax.syntax import YamlSyntax
 
 from swagtrace.yaml_schema import ElementInfo, prepareAndFinal, SwagTaceTestFormat
-from swagtrace.utils import set_variables_in_data
+from swagtrace.utils import (
+set_variables_in_data,
+get_test_client,
+load_module,
+load_variables_module,
+run_func_module,
+project_banner_information,
+configure_logging
+)
 
 
 GLOBAL_VARIABLES = {}
-
-def load_module(module_path: str):
-    path = Path(module_path)
-    
-    if not path.exists():
-        print(f"❌ File Does not exist!: {module_path}")
-        return
-    
-    module_name = path.stem
-    
-    spec = importlib.util.spec_from_file_location(module_name, path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-    
-    return module
-    
-
-
-def run_func_module(module:ModuleType, func:str, verbose:bool=False, *args, **kwargs):
-    if hasattr(module, func):
-        if verbose:
-            print(f"▶ Running {func} function: {module}")
-        getattr(module, func)(*args, **kwargs)
-    else:
-        print(f"⚠ {func} Function Does not exist in {func} file!")
-
-
-def load_variables_module(module:ModuleType, var_name: str = "VARIABLES") -> dict[str, Any]:
-
-    if hasattr(module, var_name):
-        variables = getattr(module, var_name)
-        return variables
-    else:
-        return {}
-
-
-def project_banner_information(openapi:str, info: dict):
-
-    print(f"OpenAPI: {openapi}")
-
-    for k, v in info.items():
-        print(f"{k}: {v}")
 
 
 def prepare_tests(prepare:prepareAndFinal, file:str, verbose:bool):
@@ -82,12 +45,12 @@ def prepare_tests(prepare:prepareAndFinal, file:str, verbose:bool):
     
 
 
-def run_tags_cases(host:str, tags: dict[str, list[ElementInfo]], dir:str, verbose:bool) -> int:
+def run_tags_cases(client:Client, tags: dict[str, list[ElementInfo]], dir:str, verbose:bool) -> int:
     global GLOBAL_VARIABLES
 
-    total_test = 0
     passed_test = 0
     failed_test = 0
+    start_total_time = time.time()
 
     for tag, elements in tags.items():
         print(f"Testing {tag} ...")
@@ -101,20 +64,22 @@ def run_tags_cases(host:str, tags: dict[str, list[ElementInfo]], dir:str, verbos
             path = el.path
             method = el.method
             for case in el.cases:
+                start_test_time = time.time()
                 headers = case.request_header
                 body = case.request_body
                 test_path = f"{dir}/{tag}/{case.name}.py"
                 excepted_status = case.status_code
+
                 try:
                     module = load_module(module_path=test_path)
                     run_func_module(module=module, func="prepare", verbose=verbose)
                     variables = load_variables_module(module)
 
-                    url = set_variables_in_data(f"{host}{path}", variables, GLOBAL_VARIABLES)
+                    url = set_variables_in_data(path, variables, GLOBAL_VARIABLES)
                     body = set_variables_in_data(body, variables, GLOBAL_VARIABLES)
 
                     # TODO: Handle if body is not JSON
-                    response = requests.request(method=method,
+                    response = client.request(method=method,
                                                 url=url,
                                                 headers=headers,
                                                 json=body)
@@ -122,25 +87,36 @@ def run_tags_cases(host:str, tags: dict[str, list[ElementInfo]], dir:str, verbos
                     status_code = response.status_code
 
                     assert status_code == excepted_status, f"excepted '{excepted_status}' status code but got {status_code} status code!"
+                    if case.response_content:
+                        expected_response = case.response_content
+                        response_content = response.content.decode()
+                        assert response_content == expected_response, f"excepted '{expected_response}' response but got {response_content} response!"
 
-                    total_test += 1
                     run_func_module(module=module, func="main", verbose=verbose, **{"response": response})
-                    passed_test += 1
 
                     run_func_module(module=module, func="finalize", verbose=verbose)
-                    print(f"✅ [SUCCESS] Case {case.name} Passed Successfully")
+                    duration = round((time.time() - start_test_time) * 1000, 2)
+                    passed_test += 1
+
+                    print(f"  ✅ [SUCCESS] Case {case.name} Passed Successfully ({duration}ms)")
 
                 except AssertionError as e:
-                    print(f"❌ [FAILED] test {case.name}: {e}")
+                    duration = round((time.time() - start_test_time) * 1000, 2)
+                    print(f"  ❌ [FAILED] Case {case.name} ({duration}ms)")
+                    print(f"         └── AssertionError: {e}")
                     failed_test += 1
 
                 except Exception as e:
-                    print(f"❌ [FAILED] Unexpected error: {e}")
+                    duration = round((time.time() - start_test_time) * 1000, 2)
+                    print(f"  ❌ [Error] Case {case.name} ({duration}ms)")
+                    print(f"         └── {type(e).__name__}: {e}")
                     failed_test += 1
 
+    duration = round((time.time() - start_total_time) * 1000, 2)
+
     print("=" * 60)
-    print("Test Finished")
-    print(f"Result: total: {total_test} passed: {passed_test} failed: {failed_test}")
+    print(f"Test Finished ({duration}ms)")
+    print(f"Result: total: {passed_test + failed_test} passed: {passed_test} failed: {failed_test}")
     print("=" * 60)
 
     if failed_test:
@@ -167,7 +143,13 @@ def finale_tests(final:prepareAndFinal, file:str, verbose:bool):
     run_func_module(module=module, func="main", verbose=verbose)
 
 
-def run_tests(host:str, file:str, dir:str, verbose:bool) -> int:
+def run_tests(host:str|None, app:str|None, file:str, dir:str, verbose:bool) -> int:
+
+    try:
+        client = get_test_client(host=host, app=app)
+    except Exception as e:
+        print(e)
+        return 1
 
     yaml_serialized = YamlSyntax.from_file(SwagTaceTestFormat, file)
 
@@ -175,9 +157,11 @@ def run_tests(host:str, file:str, dir:str, verbose:bool) -> int:
 
     project_banner_information(test_sections.openapi, test_sections.info)
 
+    configure_logging()
+
     prepare_tests(test_sections.prepare, str(Path(dir) / Path("prepare.py")), verbose=verbose)
 
-    exit_code = run_tags_cases(host, test_sections.tags, dir, verbose)
+    exit_code = run_tags_cases(client, test_sections.tags, dir, verbose)
 
     finale_tests(test_sections.final, str(Path(dir) / Path("final.py")), verbose=verbose)
 
