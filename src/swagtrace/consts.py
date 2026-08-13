c5ad9680331f785786c3b00ac7dfd7488f5aa119
EmadDeve20 <emaddeve20@gmail.com>
2026-08-13 10:11:48 +0330

    update template filr for prepare and finial

--- src/swagtrace/consts.py
+++ src/swagtrace/consts.py
@@ -2,26 +2,18 @@ DEFAULT_YAML_FILE = "swagtrace.yaml"
 
 DEFAULT_TEST_MODULE_FOLDER = "swagtrace_tests"
 
-PREPARE_AND_FINAL_FORMAT_FILE  = """# 1. Configuration & Variables
+PREPARE_AND_FINAL_FORMAT_FILE  = """from subprocess import CompletedProcess
+# 1. Configuration & Variables
 # Define key-value pairs to be injected into the YAML template.
 # You can set static values here or dynamically update this dictionary inside prepare().
 # NOTE: Variable names (keys) are CASE-SENSITIVE (e.g., 'DB_PORT' != 'db_port').
 VARIABLES = {}
 
-
-# 2. Preparation Phase
-# This function runs FIRST. Use it to perform any setup, fetch dynamic data,
-# or modify the 'VARIABLES' dictionary before the YAML file is generated.
-# Tip: Modify the dictionary directly (e.g., VARIABLES['MY_KEY'] = 'value').
-def prepare():
-    pass
-
-
-# 3. Main Execution Phase
+# 2. Main Execution Phase
 # This function runs as the FINAL step, right after the 'execute' command in the YAML file has finished.
 # Place your core logic or post-processing tasks here.
-def main():
-    pass
+def main(cp: CompletedProcess):
+    print(cp.stdout)
 
 """
 
