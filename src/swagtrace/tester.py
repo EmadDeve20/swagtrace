2ea75bd927bae5801c815f542adb9d0bc8162273
EmadDeve20 <emaddeve20@gmail.com>
2026-08-18 22:30:25 +0330

    create demo for runner class

--- src/swagtrace/tester.py
+++ src/swagtrace/tester.py
@@ -1,8 +1,9 @@
-
 import asyncio
 import subprocess
 import time
+from collections.abc import Coroutine
 from pathlib import Path
+from typing import Any
 
 from httpx import Client
 from yaml_syntax.syntax import YamlSyntax
@@ -14,24 +15,25 @@ from swagtrace.schemas.yaml_schema import (
     prepareAndFinal,
 )
 from swagtrace.utils import (
+    arun_func_module,
     configure_logging,
     get_test_client,
+    load_app,
     load_module,
     load_variables_module,
+    print_error_line,
     project_banner_information,
     run_func_module,
     set_variables_in_data,
-    arun_func_module,
-    load_app,
-    print_error_line,
+    function_manager,
 )
 
 GLOBAL_VARIABLES = {}
 
 
-def prepare_tests(prepare:prepareAndFinal, file:str, verbose:bool):
+def prepare_tests(prepare: prepareAndFinal, file: str, verbose: bool):
     global GLOBAL_VARIABLES
- 
+
     module = load_module(module_path=file)
 
     result = subprocess.run(prepare.execute, shell=True, text=True, capture_output=True)
@@ -41,7 +43,9 @@ def prepare_tests(prepare:prepareAndFinal, file:str, verbose:bool):
     GLOBAL_VARIABLES = load_variables_module(module=module)
 
 
-def run_tags_cases(client:Client, tags: dict[str, list[ElementInfo]], dir:str, verbose:bool) -> int:
+def run_tags_cases(
+    client: Client, tags: dict[str, list[ElementInfo]], dir: str, verbose: bool
+) -> int:
     global GLOBAL_VARIABLES
 
     passed_test = 0
@@ -52,7 +56,6 @@ def run_tags_cases(client:Client, tags: dict[str, list[ElementInfo]], dir:str, v
         print(f"Testing {tag} ...")
 
         for el in elements:
-
             if verbose:
                 print(f" summary = {el.summary}")
                 print(f"  description = {el.description}")
@@ -75,26 +78,36 @@ def run_tags_cases(client:Client, tags: dict[str, list[ElementInfo]], dir:str, v
                     body = set_variables_in_data(body, variables, GLOBAL_VARIABLES)
 
                     # TODO: Handle if body is not JSON
-                    response = client.request(method=method,
-                                                url=url,
-                                                headers=headers,
-                                                json=body)
+                    response = client.request(
+                        method=method, url=url, headers=headers, json=body
+                    )
 
                     status_code = response.status_code
 
-                    assert status_code == excepted_status, f"excepted '{excepted_status}' status code but got {status_code} status code!"
+                    assert status_code == excepted_status, (
+                        f"excepted '{excepted_status}' status code but got {status_code} status code!"
+                    )
                     if case.response_content:
                         expected_response = case.response_content
                         response_content = response.content.decode()
-                        assert response_content == expected_response, f"excepted '{expected_response}' response but got {response_content} response!"
+                        assert response_content == expected_response, (
+                            f"excepted '{expected_response}' response but got {response_content} response!"
+                        )
 
-                    run_func_module(module=module, func="main", verbose=verbose, **{"response": response})
+                    run_func_module(
+                        module=module,
+                        func="main",
+                        verbose=verbose,
+                        **{"response": response},
+                    )
 
                     run_func_module(module=module, func="finalize", verbose=verbose)
                     duration = round((time.time() - start_test_time) * 1000, 2)
                     passed_test += 1
 
-                    print(f"  ✅ [SUCCESS] Case {case.name} Passed Successfully ({duration}ms)")
+                    print(
+                        f"  ✅ [SUCCESS] Case {case.name} Passed Successfully ({duration}ms)"
+                    )
 
                 except AssertionError as e:
                     duration = round((time.time() - start_test_time) * 1000, 2)
@@ -112,7 +125,9 @@ def run_tags_cases(client:Client, tags: dict[str, list[ElementInfo]], dir:str, v
 
     print("=" * 60)
     print(f"Test Finished ({duration}ms)")
-    print(f"Result: total: {passed_test + failed_test} passed: {passed_test} failed: {failed_test}")
+    print(
+        f"Result: total: {passed_test + failed_test} passed: {passed_test} failed: {failed_test}"
+    )
     print("=" * 60)
 
     if failed_test:
@@ -122,26 +137,25 @@ def run_tags_cases(client:Client, tags: dict[str, list[ElementInfo]], dir:str, v
 
 
 # TODO: I'm going to create a best practice workflow to handle both stnc abd async projects
-# I will use this for my own test and my personal project 
+# I will use this for my own test and my personal project
 async def arun_tags_cases(
-client:Client,
-tags: dict[str, list[ElementInfo]],
-cases: list[str],
-dir:str,
-verbose:bool) -> int:
-    
+    client: Client,
+    tags: dict[str, list[ElementInfo]],
+    cases: list[str],
+    dir: str,
+    verbose: bool,
+) -> int:
+
     global GLOBAL_VARIABLES
-    
+
     passed_test = 0
     failed_test = 0
     start_total_time = time.time()
 
-    tags = {case:tags.pop(case) for case in cases if case in tags} or tags
+    tags = {case: tags.pop(case) for case in cases if case in tags} or tags
 
     for tag, elements in tags.items():
-
         for el in elements:
-
             if verbose:
                 print(f" summary = {el.summary}, flush=True")
                 print(f"  description = {el.description}", flush=True)
@@ -158,32 +172,42 @@ verbose:bool) -> int:
 
                 try:
                     module = load_module(module_path=test_path)
-                    await arun_func_module(module=module, func="prepare", verbose=verbose)
+                    await arun_func_module(
+                        module=module, func="prepare", verbose=verbose
+                    )
                     variables = load_variables_module(module)
 
                     url = set_variables_in_data(path, variables, GLOBAL_VARIABLES)
                     body = set_variables_in_data(body, variables, GLOBAL_VARIABLES)
 
                     # TODO: Handle if body is not JSON
-                    response = client.request(method=method,
-                                                url=url,
-                                                headers=headers,
-                                                json=body)
+                    response = client.request(
+                        method=method, url=url, headers=headers, json=body
+                    )
 
                     status_code = response.status_code
 
-                    assert status_code == excepted_status, f"excepted '{excepted_status}' status code but got {status_code} status code!"
+                    assert status_code == excepted_status, (
+                        f"excepted '{excepted_status}' status code but got {status_code} status code!"
+                    )
                     if case.response_content:
                         expected_response = case.response_content
                         response_content = response.content.decode()
-                        assert response_content == expected_response, f"excepted '{expected_response}' response but got {response_content} response!"
+                        assert response_content == expected_response, (
+                            f"excepted '{expected_response}' response but got {response_content} response!"
+                        )
 
-                    await arun_func_module(module=module, func="main", verbose=verbose, response=response)
+                    await arun_func_module(
+                        module=module, func="main", verbose=verbose, response=response
+                    )
 
                     duration = round((time.time() - start_test_time) * 1000, 2)
                     passed_test += 1
 
-                    print(f"  ✅ [SUCCESS] Case {case.name} Passed Successfully ({duration}ms)", flush=True)
+                    print(
+                        f"  ✅ [SUCCESS] Case {case.name} Passed Successfully ({duration}ms)",
+                        flush=True,
+                    )
 
                 except AssertionError as e:
                     duration = round((time.time() - start_test_time) * 1000, 2)
@@ -200,9 +224,13 @@ verbose:bool) -> int:
 
                 finally:
                     try:
-                        await arun_func_module(module=module, func="finalize", verbose=verbose)
+                        await arun_func_module(
+                            module=module, func="finalize", verbose=verbose
+                        )
                     except Exception as e:
-                        print(f"  ⚠️ [Warning]: Failed to run finalize function at the end of the test!")
+                        print(
+                            f"  ⚠️ [Warning]: Failed to run finalize function at the end of the test!"
+                        )
                         print(f"         └── {type(e).__name__}: {e}", flush=True)
                         print_error_line(test_path, e)
 
@@ -210,7 +238,10 @@ verbose:bool) -> int:
 
     print("=" * 60, flush=True)
     print(f"Test Finished ({duration}ms)", flush=True)
-    print(f"Result: total: {passed_test + failed_test} passed: {passed_test} failed: {failed_test}", flush=True)
+    print(
+        f"Result: total: {passed_test + failed_test} passed: {passed_test} failed: {failed_test}",
+        flush=True,
+    )
     print("=" * 60, flush=True)
 
     if failed_test:
@@ -219,8 +250,8 @@ verbose:bool) -> int:
     return 0
 
 
-def finale_tests(final:prepareAndFinal, file:str, verbose:bool):
-    
+def finale_tests(final: prepareAndFinal, file: str, verbose: bool):
+
     module = load_module(module_path=file)
 
     result = subprocess.run(final.execute, shell=True, text=True, capture_output=True)
@@ -231,12 +262,12 @@ def finale_tests(final:prepareAndFinal, file:str, verbose:bool):
 # TODO: Update Workflow if need run app first and app like FastAPI need DB
 # because app load first and if DB will generate on prepare stage, it can got and error
 def run_tests(
-host:str|None,
-app:str|None,
-file:str,
-dir:str,
-cases:list[str],
-verbose:bool,
+    host: str | None,
+    app: str | None,
+    file: str,
+    dir: str,
+    cases: list[str],
+    verbose: bool,
 ) -> int:
 
     exit_code = 0
@@ -250,17 +281,20 @@ verbose:bool,
     config = get_config()
 
     yaml_serialized = YamlSyntax.from_file(SwagTaceTestFormat, file)
-    
-    test_sections:SwagTaceTestFormat = yaml_serialized.serialized_data
+
+    test_sections: SwagTaceTestFormat = yaml_serialized.serialized_data
 
     try:
         project_banner_information(test_sections.openapi, test_sections.info)
     except Exception as e:
-        finale_tests(test_sections.final, str(Path(dir) / Path("final.py")), verbose=verbose)
+        finale_tests(
+            test_sections.final, str(Path(dir) / Path("final.py")), verbose=verbose
+        )
         return 1
 
-
-    prepare_tests(test_sections.prepare, str(Path(dir) / Path("prepare.py")), verbose=verbose)
+    prepare_tests(
+        test_sections.prepare, str(Path(dir) / Path("prepare.py")), verbose=verbose
+    )
 
     try:
         client = get_test_client(host=host, app=app)
@@ -270,13 +304,104 @@ verbose:bool,
 
     configure_logging()
 
-
     if config.project.type == "async":
-        exit_code = asyncio.run(arun_tags_cases(client, test_sections.tags, cases, dir, verbose))
+        exit_code = asyncio.run(
+            arun_tags_cases(client, test_sections.tags, cases, dir, verbose)
+        )
     else:
         exit_code = run_tags_cases(client, test_sections.tags, dir, verbose)
 
-    finale_tests(test_sections.final, str(Path(dir) / Path("final.py")), verbose=verbose)
+    finale_tests(
+        test_sections.final, str(Path(dir) / Path("final.py")), verbose=verbose
+    )
 
     return exit_code
 
+
+class TestRunner:
+    def __init__(
+        self,
+        host: str | None,
+        app: str | None,
+        file: str,
+        dir: str,
+        cases: list[str],
+        verbose: bool,
+    ):
+
+        self.exit_code = 0
+        self.app_module = None
+        self.app_name = None
+        self.cases = cases
+        self.project_dire = Path(dir)
+        self.verbose = verbose
+        self.GLOBAL_VARIABLES: dict[str, Any] = {}
+
+        if app:
+            app_module, app_name = load_app(app_path=app)
+            app = getattr(app_module, app_name)
+
+        yaml_serialized = YamlSyntax.from_file(SwagTaceTestFormat, file)
+
+        self.test_sections: SwagTaceTestFormat = yaml_serialized.serialized_data
+
+        project_banner_information(self.test_sections.openapi, self.test_sections.info)
+
+        self.client = get_test_client(host=host, app=app)
+
+        configure_logging()
+
+    async def __exec_command(self, command: str) -> asyncio.subprocess.Process:
+        return await asyncio.create_subprocess_shell(
+            command,
+            shell=True,
+        )
+
+    async def prepare_tests(self, file: str = "prepare.py"):
+
+        module = load_module(module_path=str(self.project_dire /  Path(file)))
+
+        result = await self.__exec_command(self.test_sections.prepare.execute)
+
+        await function_manager.execute_function(module.main, cp=result)
+
+        self.GLOBAL_VARIABLES = load_variables_module(module=module)
+
+    async def finale_tests(self, file: str = "final.py"):
+
+        module = load_module(module_path=str(self.project_dire /  Path(file)))
+
+        result = await self.__exec_command(self.test_sections.prepare.execute)
+
+        await function_manager.execute_function(module.main, cp=result)
+
+    async def run(self) -> int:
+        try:
+            await self.prepare_tests()
+
+            await self.finale_tests()
+
+            return 0
+
+        # TODO: make error message better for user
+        except Exception as e:
+            print(f"Failed to Run Test! Details: {e}")
+            return 1
+
+
+def run_tests(
+    host: str | None,
+    app: str | None,
+    file: str,
+    dir: str,
+    cases: list[str],
+    verbose: bool,
+) -> int:
+
+    test_runner = TestRunner(
+        host=host, app=app, file=file, cases=cases, dir=dir, verbose=verbose
+    )
+
+    exit_code = asyncio.run(test_runner.run())
+
+    return exit_code
